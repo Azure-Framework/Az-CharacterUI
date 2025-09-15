f71af1895d664b6a9be01c3dc4ab8eef69466eb3
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2025-09-14 21:40:22 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.AdminRoleId = "YOUR_DISCORD_ADMIN_ROLE_ID"
+Config.AdminRoleId = "YOUR_DISCORD_ADMIN_ROLEID"
 
 -- Set to true to enable park-anywhere functionality (Shift + F to park/unpark vehicles)
 Config.Parking = true
@@ -14,7 +14,7 @@ Config.PaycheckIntervalMinutes  = 1  -- 60 1 hour.
 
 
 -- Your Discord App ID (string). Put your app id here.
-Config.DISCORD_APP_ID = "YOUR_DISCORD_BOT_APP_ID"
+Config.DISCORD_APP_ID = "YOUR_DISCORDAPP_ID"
 
 -- Update interval in seconds
 Config.UPDATE_INTERVAL = 5
@@ -35,9 +35,25 @@ Config.EMOJIS = {
   speed    = "💨"
 }
 
--- To be done later.
+-- filename inside the resource folder to store spawns
+Config.SpawnFile = Config.SpawnFile or "spawns.json"
+
+-- map bounds used to map world XY to the 2048x2048 map image
+-- These values should match how you want the projection to work for your vertical 2048x2048 map.png
+-- Adjust to your server's world extents used for mapping.
+Config.MapBounds = Config.MapBounds or {
+  minX = -3000.0,
+  maxX = 3000.0,
+  minY = -6300.0,
+  maxY = 7000.0
+}
+
+-- optional: allow only Az-Framework admins to edit/save
+Config.RequireAzAdminForEdit = true
+
+-- Show job (ESX/QBCore) in presence? false => don't attempt to fetch job
 Config.SHOW_JOB = false
--- leave nil
+-- If you use ESX/QBCore, set framework: "esx" or "qb" or leave nil
 Config.FRAMEWORK = nil
 
 
