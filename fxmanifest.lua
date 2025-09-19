75949349ccf539a0a1635e9cb9dd8e75e8522736
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2025-09-18 17:54:46 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 author 'Azure(TheStoicBear)'
 description 'Azure CharacterUI'
-version '1.3.0'
+version '1.3.5'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -26,3 +26,4 @@ files {
     'html/config.js',
     'html/map.png'
 }
+
