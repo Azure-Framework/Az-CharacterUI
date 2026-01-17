68f447b07e8dbac4c96a3491aa8e6995dcbf509a
Stoic <112611821+TheStoicBear@users.noreply.github.com>
2026-01-17 09:28:17 -0800

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 lua54 'yes'
 author 'Azure(TheStoicBear)'
 description 'Azure CharacterUI'
-version '1.5.5'
+version '1.7.0'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -27,5 +27,6 @@ files {
     'html/config.js',
     'html/map.png'
 }
+
 
 exports('getActiveCharacter')
